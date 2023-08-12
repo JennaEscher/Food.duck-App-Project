0a19d1c4c242ba52772c26d5896e07c2a85d12ab
luna156 <v010309@naver.com>
2023-08-12 14:31:44 +0900

    즐겨찾기 cache init 완료

--- main/lib/back/data_fetch.dart
+++ main/lib/back/data_fetch.dart
@@ -1,23 +1,26 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:path_provider/path_provider.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
 import '../firebase_options.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cache_manager/cache_manager.dart';
+
 
 List<dynamic> listfood = [];
 Map name = {};
 Map category = {};
 Map trav_time = {};
 Map tag = {};
-Map listmeta = {};
+List<int> listmeta = [];
 List<String> tags=[];
 List<String> categorys=[];
 
-void makelist(var parsed_list) {
+void makelist(var parsed_list) async {
   int idx = 0;
   for (var i in parsed_list) {
     name[i["name"]] = idx;
@@ -35,7 +38,6 @@ void makelist(var parsed_list) {
       trav_time[i["trav_time"]] = <int>[];
       trav_time[i["trav_time"]].add(idx);
     }
-
     for (var j in i["tags"]) {
       if(!tags.contains(j)) tags.add(j);
       if (tag.containsKey(j)) {
@@ -44,6 +46,9 @@ void makelist(var parsed_list) {
         tag[j] = [];
         tag[j].add(idx);
       }
+    }
+    if(await InitCaches(i)==0){
+      WriteCaches(i,0);
     }
     idx++;
   }
@@ -61,33 +66,24 @@ Future<int> init(CounterStorage cs) async {
         options: DefaultFirebaseOptions.currentPlatform,
       );
       print("Firebase Initialized");
-      final storageReference = FirebaseStorage.instance
-          .refFromURL("gs://foodduck-23ca8.appspot.com/test.json");
+      final datapath = FirebaseStorage.instance
+          .refFromURL("gs://foodduck-23ca8.appspot.com/output.json");
       try {
         const oneMegabyte = 1024 * 1024;
-        final data = await storageReference.getData(oneMegabyte);
-        final String dataAsString = utf8.decode(data!);
-        print(dataAsString);
-        listfood = jsonDecode(dataAsString);
+        final data = await datapath.getData(oneMegabyte);
+        final String fooddata = utf8.decode(data!);
+        print(fooddata);
+
+        await InitCaches('food');
+        await WriteCaches('food',fooddata);
+
+        listfood = jsonDecode(fooddata);
         makelist(listfood);
         // Data for "images/island.jpg" is returned, use this as needed.
       } on FirebaseException catch (e) {
         print("{$e}");
         // Handle any errors.
       }
-      /*
-      final directory = await getApplicationDocumentsDirectory();
-
-      final path = directory.path;
-      final file = File('$path/food.json');
-      final meta_file = File('$path/meta_food.json');
-      final contents = await file.readAsString();
-      final meta = await meta_file.readAsString();
-      listfood = jsonDecode(contents);
-      listmeta = jsonDecode(meta);
-      makelist(listfood);
-      */
-
       return 0;
     } catch (e) {
       //로컬 파일이 없어서 생기는 오류(PathNotFoundException).
@@ -99,38 +95,47 @@ Future<int> init(CounterStorage cs) async {
       /*
         fetch data from firebase
       */
-      await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      );
-      print("Firebase Initialized2");
-      final storageReference = FirebaseStorage.instance
-          .refFromURL("gs://foodduck-23ca8.appspot.com/food.json");
+      print("internet not found");
       try {
-        const oneMegabyte = 1024 * 1024;
-        final data = await storageReference.getData(oneMegabyte);
-        final String dataAsString = utf8.decode(data!);
-        print(dataAsString);
+        if(await InitCaches('food') == 0){
+          throw 'data not found';
+        }
+        final String? fooddata = await ReadCaches('food');
+        listfood = jsonDecode(fooddata!);
+        makelist(listfood);
       } on FirebaseException catch (e) {
         print("{$e}");
         // Handle any errors.
       }
-      /*final directory = await getApplicationDocumentsDirectory();
-
-      final path = directory.path;
-      final file = File('$path/food.json');
-      final meta_file = File('$path/meta_food.json');
-      final contents = await file.readAsString();
-      final meta = await meta_file.readAsString();
-      listfood = jsonDecode(contents);
-      listmeta = jsonDecode(meta);
-      makelist(listfood);*/
-
       return 0;
     } catch (e) {
       print("Local Error : $e");
       return -1;
     }
   }
+}
+
+Future<int?> InitCaches(var k) async{
+  int CacheResult = 0;
+  CacheManagerUtils.conditionalCache(
+      key: k,
+      valueType: ValueType.StringValue,
+      actionIfNull: CacheResult = 0,
+      actionIfNotNull: CacheResult = 1
+  );
+  return  CacheResult;
+}
+
+
+Future<String?> WriteCaches(var k, var st) async{
+  WriteCache.setString(key: k, value: st);
+}
+
+Future<String?> ReadCaches(var k) async {
+  dynamic DasData = '';
+  var DasValue = await ReadCache.getString(key: k);
+  DasData = DasValue.toString();
+  return DasData;
 }
 
 class CounterStorage {
